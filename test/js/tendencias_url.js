64c816066224948f33b15524b005040af26a86da
Exequiel Sabatie <exequiel.sabatie@enarvi.com>
2024-06-12 12:32:49 -0300

    Js files for correct functioning

---
+++ test/js/tendencias_url.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("tendencias-form");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const params = [];
+
+    for (const pair of formData.entries()) {
+      if (pair[1] !== "") {
+        params.push(
+          `${encodeURIComponent(pair[0])}=${encodeURIComponent(pair[1])}`
+        );
+      }
+    }
+
+    const url = "https://test.reusados.net/?dir=tendencias";
+
+    if (params.length > 0) {
+      const newUrl = url + "&" + params.join("&");
+      window.location.href = newUrl;
+    } else {
+      window.location.href = url;
+    }
+  });
+});
