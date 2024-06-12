42f1b4e0d81cb31ba53f682b5036dc951f696a22
Exequiel Sabatie <exequiel.sabatie@enarvi.com>
2024-06-11 23:58:40 -0300

    Fix main header

--- layout/header.php
+++ layout/header.php
@@ -2,6 +2,6 @@
     <a class="link-light ms-3" href="<?php echo $url_base; ?>" aria-label="reUsados" title="<?php echo $header_title; ?>">
         <img src="imgs/retropc2.png" width="50px" alt="">
     </a>
-    <span><h4 class="mb-0">reUsados</h4></span>
+    <span><h4 class="mb-0"><?php echo $header_title; ?></h4></span>
     <div></div><!-- Filler -->
 </header>
