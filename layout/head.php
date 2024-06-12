ee8585b6978690aef3c933101602dcfb100fcc2b
Exequiel Sabatie <exequiel.sabatie@enarvi.com>
2024-06-11 23:56:02 -0300

    Globales para todos los enviroments, title, header

--- layout/head.php
+++ layout/head.php
@@ -1,12 +1,12 @@
 <?php
-require_once "globales/variables.php"; 
+require_once "globales/variables.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>reUsados</title>
+    <title><?php echo $navbar_title; ?></title>
     <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link href=" css/estilos.css" rel="stylesheet">
--- test/globales/variables.php
+++ test/globales/variables.php
@@ -3,3 +3,5 @@
 $url_base = getenv('URL_BASE');
 $desvioUrl = getenv('DESVIO_URL');
 $key_enlaces = getenv('KEY_ENLACES');
+$header_title = getenv('HEADER_TITLE');
+$navbar_title = getenv('NAVBAR_TITLE');
