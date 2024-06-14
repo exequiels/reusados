e4a9d8c121e0898759f2347dd5044358fcebd08b
Exequiel Sabatie <exequiel.sabatie@enarvi.com>
2024-06-14 17:52:01 -0300

    no message

--- index.php
+++ index.php
@@ -15,20 +15,27 @@ require_once 'connectar.php';
 
         <!-- Main Content -->
         <div class="container mt-2 mt-sm-4">
+
             <div class="row">
                 
-                <!-- Menus -->
-                <div class="col-lg-2">
-                    <!-- Menu -->
+            <!-- Topbar -->
+            <div class="col-10 col-sm-11 col-md-11 col-lg-12 mx-auto">
+                <div class="row">
+                    <?php require_once "layout/topbar.php"; ?>
+                </div>                
+            </div>
+            <!-- // Topbar -->
+
+                <!-- Menu -->
+                <div class="col-lg-2 col-10 col-sm-11 mx-auto">
                     <div class="row">
                         <?php require_once "layout/menu.php"; ?>
                     </div>
-                    <!-- // Menu -->
                 </div>
-                <!-- // Menus -->
+                <!-- // Menu -->
 
                 <!-- Seccion central -->
-                <div class="col-lg-10 central-item">
+                <div class="col-lg-9 central-item col-10 col-sm-11 mx-auto">
                     <?php
                     $universo = isset($_GET['dir']) ? $_GET['dir'] : 'buscador';
 $universo = filter_input(INPUT_GET, 'dir', FILTER_SANITIZE_SPECIAL_CHARS);
---
+++ test/section/mi-perfil.php
@@ -0,0 +1,8 @@
+<table class="table table-sm table-bordered border-estilo">
+    <?php include_once "globales/encabezado_de_tabla.php"; ?>
+    <tr class="p-3"> 
+        <td class="p-3" colspan="2">
+            Perfil
+        </td>
+    </tr>
+</table>
