eb75ce0d58a7bd48da356347dbed0e59412f8617
Exequiel Sabatie <exequiel.sabatie@enarvi.com>
2024-06-17 11:37:15 -0300

    Fix Js

--- js/buscador_url.js
+++ js/buscador_url.js
@@ -15,11 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
 
-<<<<<<< HEAD
     const url = "https://test.reusados.net/?dir=buscador";
-=======
-    const url = "http://localhost/reusados/?dir=buscador";
->>>>>>> b407152db69c8f4399b50513af2586a4c323d626
 
     if (params.length > 0) {
       const newUrl = url + "&" + params.join("&");
