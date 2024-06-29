2f35375a28c0cc8842bb7bcd95069406fe3129a1
Exequiel Sabatie <exequiel.sabatie@enarvi.com>
2024-06-29 14:37:04 -0300

    Base de datos creada, insert creado, testeando validaciones.

--- pagina/registrarse.php
+++ pagina/registrarse.php
@@ -12,37 +12,55 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $errors = [];
 
-    if (strlen($username) < 3 || strlen($username) > 30) {
-        $errors[] = "Mínimo 3 caracteres y máximo 30.";
+    $stmt = $pdo->prepare("SELECT COUNT(*) FROM usuarios WHERE username = ?");
+    $stmt->execute([$username]);
+    $count = $stmt->fetchColumn();
+    if ($count > 0) {
+        $errors[] = "El nombre de usuario ya está registrado.";
     }
 
-    if (strlen($fullname) < 3 || strlen($fullname) > 30) {
-        $errors[] = "Mínimo 3 caracteres y máximo 30.";
-    }
-
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = "Email no válido.";
-    }
-
-    if (!preg_match('/^(?=.*[A-Z])(?=.*\d).{8,}$/', $password)) {
-        $errors[] = "El password debe tener al menos ocho caracteres, entre ellos una mayúscula y al menos un número.";
-    }
-
-    if ($password !== $confirm_password) {
-        $errors[] = "Los passwords deben coincidir.";
+    $stmt = $pdo->prepare("SELECT COUNT(*) FROM usuarios WHERE email = ?");
+    $stmt->execute([$email]);
+    $count = $stmt->fetchColumn();
+    if ($count > 0) {
+        $errors[] = "El email ya está registrado.";
     }
 
     if (empty($errors)) {
 
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        if (strlen($username) < 3 || strlen($username) > 30) {
+            $errors[] = "Mínimo 3 caracteres y máximo 30.";
+        }
 
+        if (strlen($fullname) < 3 || strlen($fullname) > 30) {
+            $errors[] = "Mínimo 3 caracteres y máximo 30.";
+        }
 
-        $stmt = $pdo->prepare("INSERT INTO users (username, fullname, email, password) VALUES (?, ?, ?, ?)");
-        $stmt->execute([$username, $fullname, $email, $hashed_password]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors[] = "Email no válido.";
+        }
 
-        header("Location: $pagina_registrarse");
-        exit();
-    } else {
+        if (!preg_match('/^(?=.*[A-Z])(?=.*\d).{8,}$/', $password)) {
+            $errors[] = "El password debe tener al menos ocho caracteres, entre ellos una mayúscula y al menos un número.";
+        }
+
+        if ($password !== $confirm_password) {
+            $errors[] = "Los passwords deben coincidir.";
+        }
+
+        if (empty($errors)) {
+
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            $stmt = $pdo->prepare("INSERT INTO usuarios (username, fullname, email, password, created_at) VALUES (?, ?, ?, ?, NOW())");
+            $stmt->execute([$username, $fullname, $email, $hashed_password]);
+
+            echo "Registrado";
+            header("Location: $pagina_registrarse");
+            exit();
+        }
+    }
+    if (!empty($errors)) {
         foreach ($errors as $error) {
             echo "<p>$error</p>";
         }
@@ -50,14 +68,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 <table class="table table-sm table-bordered border-estilo">
-    <?php 
+    <?php
         include_once "globales/encabezado_de_tabla.php";
 
-        $num1 = rand(0, 9);
-        $num2 = rand(0, 9);
-        $captchaAnswer = $num1 + $num2;
-        $displayedAnswer = rand(0, 18);
-    ?>
+$num1 = rand(0, 9);
+$num2 = rand(0, 9);
+$captchaAnswer = $num1 + $num2;
+$displayedAnswer = rand(0, 18);
+?>
 
     <script>
         var captchaAnswer = <?= $captchaAnswer ?>;
@@ -69,30 +87,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <div class="form-group mt-3">
                     <label for="usuario">Usuario:</label>
                     <span class="text-body-tertiary">(mínimo 3 caracteres y máximo 30)</span>
-                    <input type="text" id="username" name="username" placeholder="Nickname en reUsados" maxlength="30" class="w-100" required>
+                    <input type="text" id="username" name="username" placeholder="Nickname en reUsados" minlength="3" maxlength="30" class="w-100" required>
                     <span class="validacion text-danger" style="display: none;">Algo no cuadra.</span>
                 </div>
                 <div class="form-group mt-3">
                     <label for="nombre">Nombre:</label>
                     <span class="text-body-tertiary fst-normal">(mínimo 3 caracteres y máximo 30)</span>
-                    <input type="text" id="fullname" name="fullname" placeholder="Tu nombre" maxlength="30" class="w-100" required>
+                    <input type="text" id="fullname" name="fullname" placeholder="Tu nombre" minlength="3" maxlength="30" class="w-100" required>
                     <span class="validacion text-danger" style="display: none;">Algo no cuadra.</span>
                 </div>            
                 <div class="form-group mt-3">
                     <label for="email">Correo electrónico:</label>
                     <span class="text-body-tertiary">(un email válido)</span>
-                    <input type="email" id="email" name="email" placeholder="Email" maxlength="50" class="w-100" required>
+                    <input type="email" id="email" name="email" placeholder="Email" minlength="8" maxlength="50" class="w-100" required>
                     <span class="validacion text-danger" style="display: none;">No parece válido.</span>
                 </div>
                 <div class="form-group mt-3">
                     <label for="password">Contrase&ntilde;a:</label>
                     <span class="text-body-tertiary">(mínimo ocho caracteres, entre ellos una mayúscula, un número, un caracter especial)</span>
-                    <input type="password" id="password" name="password" placeholder="Contrase&ntilde;a" maxlength="60" class="w-100" required>
+                    <input type="password" id="password" name="password" placeholder="Contrase&ntilde;a" minlength="8" maxlength="60" class="w-100" required>
                     <span class="validacion text-danger" style="display: none;">Chequea los requerimientos.</span>
                 </div>
                 <div class="form-group mt-3">
                     <label for="confirm_password">Confirmar Contrase&ntilde;a:</label>
-                    <input type="password" id="confirm_password" name="confirm_password" placeholder="Misma Contrase&ntilde;a" maxlength="60" class="w-100" required>
+                    <input type="password" id="confirm_password" name="confirm_password" placeholder="Misma Contrase&ntilde;a" minlength="8" maxlength="60" class="w-100" required>
                     <span class="validacion text-danger" style="display: none;">No coinciden la contrase&ntilde;as.</span>
                 </div>
                 <div class="form-group mt-3">
