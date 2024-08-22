e2db763089d441d30541708fcafdc8367c9a0e6f
Exequiel Sabatie <exequiel.sabatie@enarvi.com>
2024-08-22 17:57:58 -0300

    no message

--- config/cron_test.php
+++ config/cron_test.php
@@ -1,7 +1,9 @@
 <?php
 
-error_log("Esto es un test");
+$logFile = '/home/username/public_html/config/cron_test_output.log';
+
+file_put_contents($logFile, "Esto es un test a las " . date('Y-m-d H:i:s') . "\n", FILE_APPEND);
+
+error_log("El cron job 'cron_test.php' se ejecutó a las " . date('Y-m-d H:i:s'));
 
 echo "Esto es un test";
-
-file_put_contents('/home/username/public_html/config/cron_test_output.log', "Esto es un test a las " . date('Y-m-d H:i:s') . "\n", FILE_APPEND);
